cc373f3abb386bdda22a35b7ae97d1e3c82519f5
George Zhao <zhaozg@gmail.com>
2018-06-22 20:26:04 +0800

    add profile.lua

---
+++ profile.lua
@@ -0,0 +1,21 @@
+-- encoding:utf-8
+return {
+  traits = {
+    runtimePath = [[e:\Totalcmd\Tools\RIME\weasel-0.9.30\rime.dll]],
+    dataPath = [[e:\Totalcmd\Tools\RIME\weasel-0.9.30\data]],
+    userPath = [[e:\work\rime\rime_simp]],
+    name= "小狼毫",
+    code_name = 'Weasel',
+    version = '0.9.30'
+  },
+  cPoint = 'ipc://LuaIME',
+
+  ime = {
+    name="全能拼音",
+    version= "0.3.0",
+    guid= "{A381D463-9338-4FBD-B83D-66FFB03523B3}",
+    locale= "zh-CHS",
+    fallbackLocale= "zh-CN",
+    icon= "e:\\work\\zhaozg\\rime\\LuaIME\\Resource\\luaIME.ico"
+  }
+}
